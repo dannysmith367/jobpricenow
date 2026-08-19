68ec9e2967a564aec58697714c7abbe2cc4c2a67
dannysmith367 <dannysmith367@gmail.com>
2026-08-19 17:35:32 -0400

    Delete netlify/functions directory

--- netlify/functions/blog-index.mjs
+++
@@ -1,9 +0,0 @@
-import { handleBlogIndexPage } from "../../admin-logic.mjs";
-
-export default async () => {
-  const result = await handleBlogIndexPage();
-  return new Response(result.html, {
-    status: result.status,
-    headers: { "Content-Type": "text/html; charset=utf-8" },
-  });
-};
